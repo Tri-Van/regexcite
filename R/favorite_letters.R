2836acba55eda08fdc6e2c255c8467ad2bcc6e76
Tri-Van <vanminhtri95@gmail.com>
2024-01-03 15:46:45 -0800

    cahnge favrotite letter

--- R/favorite_letters.R
+++ R/favorite_letters.R
@@ -1,13 +1,21 @@
 the <- new.env(parent = emptyenv())
 the$favorite_letters <- letters[1:3]
 
+
 #' Report my favorite letters
+#'
+#' @return
 #' @export
+#'
+#' @examples
 mfl2 <- function() {
   the$favorite_letters
 }
 
 #' Change my favorite letters
+#'
+#' @param l
+#'
 #' @import dplyr
 #' @export
 set_mfl2 <- function(l = letters[24:26]) {
